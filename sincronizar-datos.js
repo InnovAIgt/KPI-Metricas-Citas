32cc4555e5e632d5ed89403326416b169199523e
InnovAIgt <innovai@red.com.gt>
2026-08-13 12:52:25 -0600

    frix

--- sincronizar-datos.js
+++ sincronizar-datos.js
@@ -2,8 +2,9 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type, x-audio-proxy",
   "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+  "Access-Control-Max-Age": "86400",
 };
 
 const BATCH_SIZE = 500;
@@ -210,7 +211,10 @@ function normalizarCallRow(registro: any) {
 
 Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
+    return new Response("ok", {
+      headers: corsHeaders,
+      status: 200,
+    });
   }
 
   try {
